7146e131c63c730546094baff221e1da9fa85b61
Drill-X <o-github@onocu.com>
2022-10-03 20:22:53 -0700

    ...

--- app/Controllers/Admin.php
+++ app/Controllers/Admin.php
@@ -21,13 +21,13 @@ class Admin extends BaseController
     {
         $users = model('UserModel');
 
-        if ($this->validate(['ueserId' => 'required|is_natural'])) {
+        if ($this->validate(['userId' => 'required|is_natural'])) {
             $id = $this->request->getPost('userId');
             $users->delete($id, true);
             return redirect()->route('admin')->withInput()->with('message', 'User successfully deleted!')->withCookies();
         }
 
-        return redirect()->route('admin')->withInput()->with('error', $this->validator->getErrors()['userId'])->withCookies();
+        return redirect()->route('admin')->withInput()->with('error', 'Something went wrong!')->withCookies();
     }
 
     public function createMagicLink($userId) 
