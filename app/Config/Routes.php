f262ca664c5ac51fa7faf47974e2465fd1e4bb1c
Drill-X <o-github@onocu.com>
2022-10-03 18:22:55 -0700

    ...

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -36,7 +36,7 @@ $routes->set404Override();
 // We get a performance increase by specifying the default
 // route since we don't have to scan directories.
 $routes->get('/', 'Home::index');
-$routes->get('/admin', 'Admin::index', ['filter' => '\App\Filters\LoggedInFilder']);
+$routes->get('/admin', 'Admin::index', ['filter' => 'isloggedin']);
 
 // Auth routing: 
 service('auth')->routes($routes, ['except' => ['login']]);
