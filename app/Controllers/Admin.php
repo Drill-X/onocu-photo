a897b28827ecc4fc23663d64a8c29a10a34750d2
Drill-X <o-github@onocu.com>
2022-10-03 21:04:02 -0700

    Added addUser().

--- app/Controllers/Admin.php
+++ app/Controllers/Admin.php
@@ -12,29 +12,67 @@ class Admin extends BaseController
         return view('admin_panel', $data);
     }
 
+    /* 
+     * Add a user to the database
+     */
     public function addUser() 
     {
-        // This function should create a user with a username and add it to the database
+        $users = model('UserModel');
+
+        $rules = [
+            'username' => config('AuthSession')->usernameValidationRules,
+            'password' => config('AuthSession')->passwordValidationRules,
+        ];
+
+        if ($this->validate($rules)) 
+        {
+            $user = new User([
+                'username' => $this->request->getPost('username'),
+                'email'    => $this->request->getPost('username'),
+                'password' => $this->request->getPost('password'),
+            ]);
+            $users->save($user);
+            
+            // To get the complete user object with ID, we need to get from the database
+            $user = $users->findById($users->getInsertID());
+            
+            // Add to default group
+            $users->addToDefaultGroup($user);
+
+            return redirect()->route('admin')->withInput()->with('message', 'User successfully created!')->withCookies();
+        }
+
+        return redirect()->route('admin')->withInput()->with('errors', $this->validator->getErrors())->withCookies();
     }
 
+    /* 
+     * Delete a user from the database (by id)
+     */
     public function delUser()
     {
         $users = model('UserModel');
 
-        if ($this->validate(['userId' => 'required|is_natural'])) {
+        if ($this->validate(['userId' => 'required|is_natural'])) 
+        {
             $id = $this->request->getPost('userId');
             $users->delete($id, true);
             return redirect()->route('admin')->withInput()->with('message', 'User successfully deleted!')->withCookies();
         }
 
-        return redirect()->route('admin')->withInput()->with('error', 'Something went wrong!')->withCookies();
+        return redirect()->route('admin')->withInput()->with('errors', $this->validator->getErrors())->withCookies();
     }
 
-    public function createMagicLink($userId) 
+    /* 
+     * Create a magic link to login as a user without using a password
+     */
+    public function getLoginLink() 
     {
 
     }
 
+    /* 
+     * Modify and existing user
+     */
     public function modUser()
     {
         return;
