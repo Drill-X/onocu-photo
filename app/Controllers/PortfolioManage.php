e8065dc20531d9be432ff470abf3d04d73ea4071
Drill-X <o-github@onocu.com>
2022-10-06 22:09:53 -0700

    Another commit to get unfinished stuff to another device.

--- app/Controllers/PortfolioManage.php
+++ app/Controllers/PortfolioManage.php
@@ -13,16 +13,27 @@ class PortfolioManage extends BaseController
     }
 
     /*
-     * Uploads a photo
+     * Uploads a photo via AJAX request
      * 
      * Specifically, this function will
      * - Validate the file (ensure the file ends in a valid extension, proper name, etc.)
      * - Copy the file to the user's public folder (which will have to be created when the user is)
+     * - Add the photo to the database
      * - Maybe there should be some JS on the frontend to update the preview? 
      */
     public function uploadPhoto()
     {
-        return;
+        $user = auth()->user();
+        $uploadDirectory = FCPATH.'uploads/'.$user->getEmail();
+        $response_array; // This will hold the response status and message
+
+        // Creates the user's upload directory if it doesn't already exist, and return an error if that operation fails
+        if (!(is_dir($uploadDirectory) || mkdir($uploadDirectory, 0744)))
+        {
+            $response_array['status'] = 'error';
+        }
+
+
     }
 
     /*
--- app/Views/admin_panel.php
+++ app/Views/admin_panel.php
@@ -5,72 +5,76 @@
 <?= $this->section('title') ?><?= $title ?> <?= $this->endSection() ?>
 
 <?= $this->section('main') ?>
-    <?php /* Alerts 'n stuff (copied from login.php) */ ?>
-    <?php if (session('error') !== null) : ?>
-        <div class="alert alert-danger" role="alert"><?= session('error') ?></div>
-    <?php elseif (session('errors') !== null) : ?>
-        <div class="alert alert-danger" role="alert">
-            <?php if (is_array(session('errors'))) : ?>
-                <?php foreach (session('errors') as $error) : ?>
-                    <?= $error ?>
-                    <br>
+
+    <div class="container-fluid">
+        <?php /* Alerts 'n stuff (copied from login.php) */ ?>
+        <?php if (session('error') !== null) : ?>
+            <div class="alert alert-danger" role="alert"><?= session('error') ?></div>
+        <?php elseif (session('errors') !== null) : ?>
+            <div class="alert alert-danger" role="alert">
+                <?php if (is_array(session('errors'))) : ?>
+                    <?php foreach (session('errors') as $error) : ?>
+                        <?= $error ?>
+                        <br>
+                    <?php endforeach ?>
+                <?php else : ?>
+                    <?= session('errors') ?>
+                <?php endif ?>
+            </div>
+        <?php endif ?>
+
+        <?php if (session('message') !== null) : ?>
+        <div class="alert alert-success" role="alert"><?= session('message') ?></div>
+        <?php endif ?>
+
+        
+        <section class="user-list">
+            <h3>User list:</h3>
+            <table class="table table-striped">
+                <thead>
+                    <tr>
+                        <th>Id</th>
+                        <th>Email</th>
+                        <th>Username</th>
+                        <th>Delete user?</th>
+                    </tr>
+                </thead>
+                <tbody>
+                <?php foreach ($user_list as $user): ?>
+                    <tr>
+                        <th scope="row"><?= esc($user->id) ?></th>
+                        <td>Email: <?= esc($user->email) ?></td>
+                        <td>Username: <?= esc($user->username) ?></td>
+                        <td>
+                            <form action="/admin/deluser" method="post">
+                                <?= csrf_field() ?>
+                                <input type="hidden" name="userId" value="<?= esc($user->id) ?>">
+                                <input type="submit" name="submit" value="Delete user">
+                            </form>
+                        </td>
+                    </tr>
                 <?php endforeach ?>
-            <?php else : ?>
-                <?= session('errors') ?>
-            <?php endif ?>
-        </div>
-    <?php endif ?>
-
-    <?php if (session('message') !== null) : ?>
-    <div class="alert alert-success" role="alert"><?= session('message') ?></div>
-    <?php endif ?>
+                </tbody>
+            </table>
+        </section>
+        
 
 
-    <section class="user-list">
-        <h3>User list:</h3>
-        <table class="table table-striped">
-            <thead>
-                <tr>
-                    <th>Id</th>
-                    <th>Email</th>
-                    <th>Username</th>
-                    <th>Delete user?</th>
-                </tr>
-            </thead>
-            <tbody>
-            <?php foreach ($user_list as $user): ?>
-                <tr>
-                    <th scope="row"><?= esc($user->id) ?></th>
-                    <td>Email: <?= esc($user->email) ?></td>
-                    <td>Username: <?= esc($user->username) ?></td>
-                    <td>
-                        <form action="/admin/deluser" method="post">
-                            <?= csrf_field() ?>
-                            <input type="hidden" name="userId" value="<?= esc($user->id) ?>">
-                            <input type="submit" name="submit" value="Delete user">
-                        </form>
-                    </td>
-                </tr>
-            <?php endforeach ?>
-            </tbody>
-        </table>
-    </section>
+        <section class="add-user">
+            <form action="/admin/adduser" method="post">
+                <?= csrf_field() ?>
+                <label for="username">Username</label>
+                <input type="input" name="username">
 
+                <label for="password">Password</label>
+                <input type="input" name="password">
 
-    <section class="add-user">
-        <form action="/admin/adduser" method="post">
-            <?= csrf_field() ?>
-            <label for="username">Username</label>
-            <input type="input" name="username">
+                <label for="password_confirm">Confirm password</label>
+                <input type="input" name="password_confirm">
 
-            <label for="password">Password</label>
-            <input type="input" name="password">
-
-            <label for="password_confirm">Confirm password</label>
-            <input type="input" name="password_confirm">
-
-            <input type="submit" name="submit" value="Create user">
-        </form>
-    </section>
+                <input type="submit" name="submit" value="Create user">
+            </form>
+        </section>
+    </div>
 
 <?= $this->endSection() ?>
